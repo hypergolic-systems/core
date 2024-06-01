a19359de8fc43198d82355608eaad0e6153b831a
Alex Rickabaugh <alx+alxhub@alxandria.net>
2024-05-31 21:32:06 -0700

    impl: simple engine producing thrust
    Implements a very simple engine that, when started, generates thrust in
    proportion to the throttle. This is incredibly basic, but will serve as a
    building block to start adding in more layers of engine functionality.
    
    Hilariously, surface effects on the engine model do seem to work (it glows red)
    though I don't fully understand why.

--- mod/Game/PartModules/HgPartEngine.cs
+++ mod/Game/PartModules/HgPartEngine.cs
@@ -1,19 +1,42 @@
+using System.Collections.Generic;
+using System.Linq;
 using Hgs.Core.Virtual;
+using UnityEngine;
 
 public class HgPartEngine : HgVirtualPartModule {
 
 
- 
+  [KSPField]
+  public float maxThrust = 0f;
+
+  [KSPField]
+  private string thrustVectorTransformName = "thrustTransform";
+
+  private List<Transform> thrustTransforms;
+
   public override void InitializeComponents() {
   }
 
   public override void OnStart(StartState state) {
     base.OnStart(state);
-
-    // FX Group Setup
+    thrustTransforms = this.thrustTransforms = this.part.FindModelTransforms(this.thrustVectorTransformName).ToList();
   }
 
-  [KSPEvent(guiActive = true, guiName = "Activate")]
-   public void Activate() {
-   }
+  bool active = false;
+
+  [KSPEvent(guiActive = true, guiActiveEditor = false, guiName = "Test!")]
+  public void Activate() {
+    active = true;
+  }
+
+  public void FixedUpdate() {
+    if (active) {
+      float finalThrust = maxThrust * this.vessel.ctrlState.mainThrottle;
+      var multiplier = 1f / thrustTransforms.Count;
+      foreach (var thrustTransform in thrustTransforms) {
+        var thrust = -thrustTransform.forward * finalThrust * multiplier;
+        this.part.AddForceAtPosition(thrust, (Vector3d) thrustTransform.position);
+      }
+    }
+  }
 }
